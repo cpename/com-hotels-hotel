868d791c1bdf66d5f75aca8190e3793508c2e9c3
cpena <cristianpenamesina@gmail.com>
2025-09-09 19:59:53 -0300

    configuracion cross origin

--- src/main/java/com/hotelsbook/hotel/controllers/HotelController.java
+++ src/main/java/com/hotelsbook/hotel/controllers/HotelController.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ import com.hotelsbook.hotel.services.HotelService;
 
 import lombok.extern.slf4j.Slf4j;
 
+@CrossOrigin(origins = "http://localhost:4200/" )
 @RestController
 @RequestMapping("/api/hotels")
 @Slf4j
